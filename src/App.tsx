0878063a978de9c1f83449d37227ae0fb9474a52
kacper <kacper@matexam.com>
2023-09-12 11:22:32 +0200

    Left Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Button, ChakraProvider } from "@chakra-ui/react"
 
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config,
+   fonts: {
+    heading: `'Open Sans', sans-serif`,
+    body: `'Raleway', sans-serif`,
+  }, })
+
 function App() {
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button>Start</Button>
     </ChakraProvider>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LeftMenu from './components/shared/leftMenu.tsx';
+
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <LeftMenu />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
