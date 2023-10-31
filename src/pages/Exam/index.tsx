812d2e73f74e97743901b48572055363d0f9199b
kacper <kacperu@matexam.com>
2023-10-31 11:23:24 +0100

    ExamView - Main Info

--- src/pages/Exam/index.tsx
+++ src/pages/Exam/index.tsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { Box, Text } from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Badge, Box, Table, TableCaption, TableContainer, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { ExamView, StudentAssignedToRoom } from "../../interfaces/exams";
 import ExamsAPIService from "../../services/api/exams/ExamsAPIService";
@@ -10,8 +10,15 @@ function ExamPage() {
 
     const [examView, setExamView] = useState<ExamView>();
 
+    const navigate = useNavigate();
+
     useEffect(()=>{
-        getExam(1)
+        if(examid == undefined){
+            navigate("/error")
+        }
+        else if(examid != undefined){
+            getExam(parseInt(examid))
+        }
     }, [])
 
     const getExam = (examid: number) => {
@@ -24,11 +31,64 @@ function ExamPage() {
     }
 
     return(
-        <>
+        <Box padding="1.5vw">
             <Box>
-                <Text>{examid}</Text>
+                <Text fontSize="4xl">
+                    {examView?.name}
+                    <Badge marginLeft="0.5vw" fontSize="2xl">{examView?.type == "basic" ? "Podstawowy" : (examView?.type == "extended" ? "Rozszerzony" : "Ustny")}</Badge>
+                </Text>
+                <Text>
+                    Od: <b></b>
+                    {
+                        examView?.startTime &&
+                            new Date(examView?.startTime?.toString()).toLocaleString("pl-PL", {year: "numeric", month: "2-digit", day: "2-digit",hour: '2-digit', minute: "2-digit", weekday: "short"})
+                    }
+                </Text>
+                <Text>
+                    Do: <b></b>
+                    {
+                        examView?.endTime &&
+                            new Date(examView?.endTime?.toString()).toLocaleString("pl-PL", {year: "numeric", month: "2-digit", day: "2-digit",hour: '2-digit', minute: "2-digit", weekday: "short"})
+                    }
+                </Text>
             </Box>
-        </>
+            <Box>
+                <TableContainer width="44vw">
+                    <Table variant='striped' colorScheme='teal'>
+                        <TableCaption>Nieprzypisani uczniowie</TableCaption>
+                        <Thead>
+                        <Tr>
+                            <Th>Nr w dzienniku</Th>
+                            <Th>Oddział</Th>
+                            <Th>Nazwisko</Th>
+                            <Th>Imię</Th>
+                            <Th>PESEL</Th>
+                        </Tr>
+                        </Thead>
+                        <Tbody>
+                            {examView?.unassignedStudents.map((result)=>
+                                <Tr key={result.PESEL}>
+                                    <Td>{result.ordinalNumber}</Td>
+                                    <Td>{result.department}</Td>
+                                    <Td>{result.surname}</Td>
+                                    <Td>{result.name}</Td>
+                                    <Td>{result.PESEL}</Td>
+                                </Tr>
+                            )}
+                        </Tbody>
+                        <Tfoot>
+                        <Tr>
+                            <Th>Nr w dzienniku</Th>
+                            <Th>Oddział</Th>
+                            <Th>Nazwisko</Th>
+                            <Th>Imię</Th>
+                            <Th>PESEL</Th>
+                        </Tr>
+                        </Tfoot>
+                    </Table>
+                </TableContainer>
+            </Box>
+        </Box>
     )
 }
 
--- src/services/api/exams/ExamsAPIService.ts
+++ src/services/api/exams/ExamsAPIService.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import { ExamBody, ExamItem, ExamView, ExamsList, StudentAssignedToRoom } from '../../../interfaces/exams'
+import { ExamItem, ExamView, ExamsList, StudentAssignedToRoom } from '../../../interfaces/exams'
 import { AuthService } from '../../auth/AuthService'
 
 const ExamsAPIService = {
     getExams: function() {
         return axios.get<ExamsList>(`${import.meta.env.VITE_API_URL}${import.meta.env.VITE_GET_EXAMS}`)
     },
-    addExam: function(examBody: ExamBody) {
+    addExam: function(examBody: ExamItem) {
         return axios.post<ExamItem>(`${import.meta.env.VITE_API_URL}${import.meta.env.VITE_GET_EXAMS}`, examBody)
     },
     getExam: function(id: number) {
