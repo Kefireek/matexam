f34f79dc86e0d9b6fdce40abce73b21ddf9b1177
Kamil <meff2000@gmail.com>
2023-11-07 11:12:57 +0100

    updated csvModal

--- src/components/csvModal.tsx
+++ src/components/csvModal.tsx
@@ -9,7 +9,15 @@ import {
     FormControl,
     FormErrorMessage,
     Button,
+    TableContainer,
+    Thead,
+    Table,
+    Tr,
+    Th,
+    Tbody,
+    Td,
 } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
 import { useForm } from "react-hook-form";
 
 
@@ -21,6 +29,31 @@ const CsvModal = () => {
         formState: {errors, isSubmitting}
     } = useForm();
 
+    const [data, setData] = useState<String>();
+
+    const [headers, setHeaders] = useState<String[]>();
+    const [rows, setRows] = useState<String[][]>();
+
+    const acceptedExtension = ["csv"];
+
+    const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        if(e.target.files){
+            try{
+                const file = e.target.files[0];
+                const fileUrl = URL.createObjectURL(file);
+                const res = await fetch(fileUrl).then(async (resp) => {
+                    var data = await resp.text();
+                    var data_ = data.split("\n");
+                    var _data = data_.map((row) => row.split(","));
+                    setHeaders(_data[0]);
+                    setRows(_data.slice(1));
+                })
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+    }
     const onSubmit = () => {
         
     }
@@ -34,16 +67,38 @@ const CsvModal = () => {
                 <form id="csv-form" onSubmit={handleSubmit(onSubmit)}>
                     <FormControl isInvalid={errors.file?.message != null}>
                         <FormLabel> Dodaj plik </FormLabel>
-                        <Input 
+                        <Input size=""
                             type="file"
                             {...register(
                                 'file'
                             )}
+                            accept=".csv"
+                            onChange={handleChange}
                         />
-                        <FormErrorMessage>  </FormErrorMessage>
+                        <FormErrorMessage> </FormErrorMessage>
                     </FormControl>
                 <Button type="submit" id="csv-form" isLoading={isSubmitting} colorScheme='teal'>Zatwierdź!</Button>
                 </form>
+                <TableContainer>
+                    <Table variant='simple' colorScheme='teal'>
+                        <Thead>
+                            <Tr>
+                                {headers?.map((header, i) => (
+                                    <Th key={i}> {header}</Th>
+                                ))}
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {rows?.map((rows, i) => (
+                                <Tr key={i}>
+                                    {rows?.map((data, i) => (
+                                        <Td key={i}> {data}</Td>
+                                    ))}
+                                </Tr>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
             </ModalBody>
         </ModalContent>
     )
