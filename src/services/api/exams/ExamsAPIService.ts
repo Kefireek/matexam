98b817a43e28cc87fd1be6d5bb5ede2eb5f59831
Kamil <meff2000@gmail.com>
2023-11-10 11:07:22 +0100

    fix

--- src/services/api/exams/ExamsAPIService.ts
+++ src/services/api/exams/ExamsAPIService.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
-import { ExamBody, ExamItem, ExamsList, StudentAssignedToRoom } from '../../../interfaces/exams'
+import { ExamItem, ExamView, ExamsList, StudentAssignedToRoom } from '../../../interfaces/exams'
+import { ExamFormModel } from '../../../components/examForm'
 
 const ExamsAPIService = {
     getExams: function() {
         return axios.get<ExamsList>(`${import.meta.env.VITE_API_URL}${import.meta.env.VITE_GET_EXAMS}`)
     },
-    addExam: function(examBody: ExamItem) {
+    addExam: function(examBody: ExamFormModel) {
         return axios.post<ExamItem>(`${import.meta.env.VITE_API_URL}${import.meta.env.VITE_GET_EXAMS}`, examBody)
     },
     getExam: function(id: number) {
