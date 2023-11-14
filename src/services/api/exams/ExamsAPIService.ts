a199accb1052d0db9b88257d768e8dcadfcbbebf
kacper <kacperu@matexam.com>
2023-11-14 08:41:34 +0100

    ExamService Fix

--- src/services/api/exams/ExamsAPIService.ts
+++ src/services/api/exams/ExamsAPIService.ts
@@ -9,7 +9,7 @@ const ExamsAPIService = {
         return axios.post<ExamItem>(`${import.meta.env.VITE_API_URL}${import.meta.env.VITE_GET_EXAMS}`, examBody)
     },
     getExam: function(id: number) {
-        return axios.get<StudentAssignedToRoom[]>(`${import.meta.env.VITE_GET_EXAMS}${id}`)
+        return axios.get<ExamView>(`${import.meta.env.VITE_GET_EXAMS}${id}`)
     },
     deleteExam: function(id: number) {
         return axios.delete<void>(`${import.meta.env.VITE_API_URL}${import.meta.env.VITE_GET_EXAMS}${id}`)
