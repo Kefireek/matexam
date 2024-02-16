98fb12a372c2cda8a667f4c848a5b596a604381e
Kamil <meff2000@gmail.com>
2024-02-16 11:23:41 +0100

    updated searchBar

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,14 +1,40 @@
-import { Box, Button, Input, InputGroup, InputLeftAddon, InputRightAddon, Select } from "@chakra-ui/react";
+import { 
+  Box, 
+  Input, 
+  InputGroup,
+  InputLeftAddon, 
+  Select, 
+  Spinner 
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 const SearchBar = () => {
+    const [searchValue, setSearchValue] = useState<string>('');
+    const [debouncedSearchValue, setDebouncedSearchValue] = useState<string>('');
+
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    };
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedSearchValue(searchValue);
+            console.log(searchValue);
+
+
+        }, 1000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [searchValue, 1000]);
+
   return (
       <Box display="flex">
         <InputGroup>
-          <Input placeholder="Szukaj..." variant="filled" />
-          <InputRightAddon>
-            <Button background="transparent" variant="solid" >Szukaj</Button>
-          </InputRightAddon>
-        </InputGroup>  
+          <Input placeholder="Szukaj..." variant="filled" onChange={(e) => onSearchChange(e)} value={searchValue}/>
+          <InputLeftAddon backgroundColor="gray.800" border="none">
+            { <Spinner size="sm" opacity={(searchValue != debouncedSearchValue) ? 1 : 0 }/>}
+          </InputLeftAddon>
+        </InputGroup>
       </Box>
   )
 }
