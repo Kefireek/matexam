3d1e4cca1a9e967cbf08957a143d0cff3e185381
Kamil <72623198+Skriptoo@users.noreply.github.com>
2024-02-28 22:11:08 +0100

    button fix

--- src/components/examForm.tsx
+++ src/components/examForm.tsx
@@ -26,7 +26,7 @@ export type ExamFormModel = {
     type: ExamType
 }
 
-const ExamForm = ({refreshExams, onCloseExam, examBody}: {refreshExams: () => void, onCloseExam: () => () => void,  examBody?: ExamItem}) => {
+const ExamForm = ({refreshExams, onCloseExam, examBody}: {refreshExams: () => void, onCloseExam: () => void,  examBody?: ExamItem}) => {
 
     
     const { 
